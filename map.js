73481ac98fbfeb0978be4fa2c305eeb4e2eeec2a
shuntarohata <shuntaro.hata@gmail.com>
2025-08-14 17:09:16 +0900

    readjust

--- map.js
+++ map.js
@@ -1,9 +1,8 @@
 // 日本を中心に表示（Web Mercator投影）
   //地図の中心地を定義
-  var mymap = L.map('mapid').setView([35, 140], 1);
+  var mymap = L.map('mapid').setView([35, 140], 0);
   //var mymap = L.map('mapid').setView([37.85, 138.8], 9);
   //タイルレイヤーを取得
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>',
     maxZoom: 18}).addTo(mymap);
-    mymap.fitWorld();
