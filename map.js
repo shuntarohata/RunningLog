6e30131b694f1ed17feacc00be2a007fb032d05b
shuntarohata <shuntaro.hata@gmail.com>
2025-08-14 16:36:16 +0900

    change to mymap

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
 // 日本を中心に表示（Web Mercator投影）
-var map = L.map('map').setView([35, 140], 2);
-
-// OpenStreetMapタイルを読み込み
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+  //地図の中心地を定義
+  //var mymap = L.map('mapid').setView([37.85, 138.8], 9);
+  var mymap = L.map('mapid').setView([35, 140], 2);
+  //タイルレイヤーを取得
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>',
+    maxZoom: 18}).addTo(mymap);
